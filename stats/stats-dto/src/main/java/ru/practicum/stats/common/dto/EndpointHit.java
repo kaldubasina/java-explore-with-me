ba6e315c0aa62eb43cefc82ec9b3512a4ddd691d
kaldubasina <kaldubasina@yandex.ru>
2023-09-18 17:15:41 +0300

    fix issues

--- stats/stats-dto/src/main/java/ru/practicum/stats/common/dto/EndpointHit.java
+++ stats/stats-dto/src/main/java/ru/practicum/stats/common/dto/EndpointHit.java
@@ -1,11 +1,14 @@
 package ru.practicum.stats.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+
+import static ru.practicum.stats.common.utils.Constant.DATE_TIME_FORMAT;
 
 @Data
 @Builder
@@ -17,5 +20,6 @@ public class EndpointHit {
     @NotBlank
     private String ip;
     @NotNull
+    @JsonFormat(pattern = DATE_TIME_FORMAT)
     private LocalDateTime timestamp;
 }
